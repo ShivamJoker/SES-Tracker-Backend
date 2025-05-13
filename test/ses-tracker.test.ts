47aeb120d7201a058a79275410d3b420ad4663b6
Shivam <singhshivamkr@gmail.com>
2025-05-14 00:05:27 +0530

    Initial commit

---
+++ test/ses-tracker.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SesTracker from '../lib/ses-tracker-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ses-tracker-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SesTracker.SesTrackerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
